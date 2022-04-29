44d1c52b94f89e1d7af128257d290c9de02ac1a1
Marcos <marcosfparreiras@gmail.com>
2022-04-29 22:49:30 +0200

    Refactor code based on linter and add proper documentation to the solution

--- lib/hacker_rank/recursive_digit_sum.rb
+++ lib/hacker_rank/recursive_digit_sum.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 module HackerRank
+  # This class implements the solution to the RecursiveDigitSum problem from
+  # Hacker Rank.
+  #
+  # The link for the problem in HackerRank is the following:
+  # https://www.hackerrank.com/challenges/recursive-digit-sum/problem
   class RecursiveDigitSum
     # string_number: String
     # repetition_times: Integer
@@ -7,6 +14,19 @@ module HackerRank
       @repetition_times = repetition_times
     end
 
+    # Complexity (n: number of chars in the string)
+    # Runtime: O(n)
+    # Space complexity: O(n)
+    #
+    # How does it work?
+    # The idea is to get the super digit for the string_number itself, and we
+    # do it by getting the sum of all of its digits, and call the method
+    # recursively with that sum (as string), until we get to one single digit.
+    #
+    # After that, we simply calculate the super digit from the value got
+    # multiplied by the repetition_times (the mathematical property ensures
+    # that this will work for any case)
+    #
     # returns Integer
     def super_digit
       number_superdigit = super_digit_recursive(@string_number)
--- spec/hacker_rank/recursive_digit_sum_spec.rb
+++ spec/hacker_rank/recursive_digit_sum_spec.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
 
 require 'hacker_rank/recursive_digit_sum'
 
-
 describe HackerRank::RecursiveDigitSum do
-	subject { HackerRank::RecursiveDigitSum.new(string_number, repetition_times) }
+  subject { HackerRank::RecursiveDigitSum.new(string_number, repetition_times) }
 
-	describe '#super_digit' do
+  describe '#super_digit' do
     context 'when initial number has only one digit' do
       let(:string_number) { '2' }
       let(:repetition_times) { 3 }
@@ -42,13 +42,10 @@ describe HackerRank::RecursiveDigitSum do
 
     context 'when entry is really big' do
       let(:string_number) { (10**100_000).to_s }
-      let(:repetition_times) { 10^5 }
+      let(:repetition_times) { 10 ^ 5 }
       let(:expected_value) { 6 }
 
       it { expect(subject.super_digit).to eq(expected_value) }
     end
   end
 end
-
-
-
