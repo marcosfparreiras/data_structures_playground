49a41933ad243c877050612b58204b358e321422
Marcos <marcosfparreiras@gmail.com>
2022-05-11 19:34:07 +0200

    Add explanation to method applied to solve the problem

--- lib/leet_code/count_sorted_vowel_strings.rb
+++ lib/leet_code/count_sorted_vowel_strings.rb
@@ -14,6 +14,23 @@ module LeetCode
       @num = num
     end
 
+    # Complexity (n being the number)
+    # Time complexity: O(n) - we need (n - 2) * 5 operations
+    # Space complexity: O(1)
+    #
+    # How does it work?
+    # We solve the problem using Dynamic Programming, following the logic that
+    # for every number n + 1, each letter will have the following number of
+    # possible words:
+    # - a: the sum of words starting from a, e, i, o, u
+    # - e: the sum of words starting from e, i, o, u
+    # - i: the sum of words starting from i, o, u
+    # - o: the sum of words starting from o, u
+    # - u: the sum of words starting from u
+    #
+    # We iterate 2 - num times, because the array arr in initialized considering
+    # n = 1
+    #
     # return Integer
     def count
       return 0 if @num == 0
